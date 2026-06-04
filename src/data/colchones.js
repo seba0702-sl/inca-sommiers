e3b1c5b145e2e4d7c1b5295095eaf4e3fe9ef23a
seba0702-sl <sebafrisso1984@gmail.com>
2026-06-04 13:19:37 -0300

    Actualizacion-zomm otro productos

--- src/data/colchones.js
+++ src/data/colchones.js
@@ -312,6 +312,119 @@ export const colchones = [
         pdf: "/pdf/Suavegom/Esplendid.pdf"
 
   },
+  {
+    id: 20,
+    brand: "Piero",
+    model: "Corona Real",
+    logo: "/images/marcas/Piero.png",
+    description: "Revestimiento en tela importada de primera calidad para un toque elegante.",
+    type:"Resortes",
+    sizes: [
+      "Todas las medidas",
+    ],
+
+    image: "/images/colchones/Piero/CoronaR.jpg",
+
+        pdf: "/pdf/Piero/Corona.pdf"
+
+  },
+  {
+    id: 21,
+    brand: "Piero",
+    model: "Foam",
+    logo: "/images/marcas/Piero.png",
+    description: "Exclusiva espuma de poliuterano con proceso Air Flow (permeabilidad de aire controlada) integrada por millones de diminutas celdas interconectadas de alta elasticidad y duración.",
+    type:"Espuma",
+    sizes: [
+      "Todas las medidas",
+    ],
+
+    image: "/images/colchones/Piero/Foam.jpg",
+
+        pdf: "/pdf/Piero/Foam.pdf"
+
+  },
+  {
+    id: 22,
+    brand: "Piero",
+    model: "Nova",
+    logo: "/images/marcas/Piero.png",
+    description: "Explora al nuevo Nova: un avance en tecnología de descanso. Diseñado para optimizar tu rendimiento diario, el Nova combina innovación de vanguardia con una precisión ergonómica, asegurando que puedas dar tu mejor versión dia a dia.",
+    type:"Espuma",
+    sizes: [
+      "Todas las medidas",
+    ],
+
+    image: "/images/colchones/Piero/Nova.png",
+
+        pdf: "/pdf/Piero/Nova.pdf"
+
+  },
+    {
+    id: 23,
+    brand: "Piero",
+    model: "Spring",
+    logo: "/images/marcas/Piero.png",
+    description: "Los resortes continuos de la estructura Ultra Coil de Piero están totalmente entrelazados, sin nudos y ataduras. Esto permite que la estructura se vuelva mas firme y la durabilidad del colchón sea mayor a los otros sistemas. ",
+    type:"Resortes",
+    sizes: [
+      "Todas las medidas",
+    ],
+
+    image: "/images/colchones/Piero/Spring.jpg",
+
+        pdf: "/pdf/Piero/Spring.pdf"
+
+  },
+   {
+    id: 24,
+    brand: "Piero",
+    model: "Bahia",
+    logo: "/images/marcas/Piero.png",
+    description: "Excelente propuesta de descanso que resume la tecnología exclusiva de Piero. Posee resortes continuos, ofrece durabilidad, confort en sus mantas internas, y un tapizado en Jackard exclusivo.  ",
+    type:"Resortes",
+    sizes: [
+      "Todas las medidas",
+    ],
+
+    image: "/images/colchones/Piero/Bahia.jpg",
+
+        pdf: "/pdf/Piero/Bahia.pdf"
+
+  },
+  {
+    id: 25,
+    brand: "Piero",
+    model: "Fleur",
+    logo: "/images/marcas/Piero.png",
+    description: "Diseño renovado para un colchón que ofrece una buena combinación entre densidad, altura y tapizado Jackard. Con sistema europillow para mayor comodidad en el descanso.",
+    type:"Espuma",
+    sizes: [
+      "Todas las medidas",
+    ],
+
+    image: "/images/colchones/Piero/Fleur.jpg",
+
+        pdf: "/pdf/Piero/Fleur.pdf"
+
+  },
+  {
+    id: 26,
+    brand: "Piero",
+    model: "Body",
+    logo: "/images/marcas/Piero.png",
+    description: "Su exclusiva espuma con proceso Air Flow — sistema de celdas interconectadas de alta elasticidad — permite que el aire circule constantemente, evitando la sensación calurosa típica de los colchones convencionales. Nuevo diseño, misma calidad Piero.",
+    type:"Espuma",
+    sizes: [
+      "Todas las medidas",
+    ],
+
+    image: "/images/colchones/Piero/Body.jpg",
+
+        pdf: "/pdf/Piero/Body.pdf"
+
+  },
+
   //Suavestar
   {
     id: 30,
